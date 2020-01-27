b06bc6854ed27a6e23f545ee8e2e15f962bce853
Javier <jagi.ccs@gmail.com>
2020-01-27 15:09:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// import { isMainThread } from "worker_threads";
+
 // Write your code here!
+
+// document.querySelector("#main").remove();
+
+const newHeader = document.querySelector("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Javier is the champion";
+newHeader.className = "victory";
+
+ const element = document.querySelector("#main").remove();
+// element.remove()
+

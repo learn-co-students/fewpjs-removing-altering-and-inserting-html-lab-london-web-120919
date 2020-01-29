17f3cabd3536daf3e728ab264a30b78a3c33c881
rmiri <re_miriuk@hotmail.com>
2020-01-29 08:56:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+main.remove()
+
+
+
+
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+ 
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
+newHeader.className = "victory";
+
+
+
